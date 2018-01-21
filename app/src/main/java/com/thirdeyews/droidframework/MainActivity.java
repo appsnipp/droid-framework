f91fd8e1303887c5e9b553e4cdfbf6496523677a
kapil <kapil@thirdeyews.com>
2018-01-22 05:18:42 +0530

    uploded

---
+++ app/src/main/java/com/thirdeyews/droidframework/MainActivity.java
@@ -0,0 +1,20 @@
+package com.thirdeyews.droidframework;
+
+/**
+ * Created by kapil on 22/01/18.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
